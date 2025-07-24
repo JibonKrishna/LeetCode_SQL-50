62f361bfb912b01830a8ba2142c5f5ea9d1c61a9
JibonKrishna <jibon.mbox@outlook.com>
2025-07-24 19:15:46 +0600

    SQL-50

--- LeetCode_SQL_50/LeetCode_1321.sql
+++ LeetCode_SQL_50/LeetCode_1321.sql
@@ -34,6 +34,40 @@ final AS(
 SELECT visited_on,rolling_sum AS amount,ROUND(rolling_average,2) AS average_amount
 FROM final
 WHERE rolling_sum IS NOT NULL
+
+
+
+--Here is a different implementation wihout the case statement
+-- Write your PostgreSQL query statement below
+WITH cte AS(
+    SELECT visited_on,SUM(amount) AS amount
+    FROM Customer
+    GROUP BY visited_on
+    ORDER BY visited_on
+),
+final AS(
+    SELECT visited_on,
+
+    COUNT(*) OVER (
+      ORDER BY visited_on
+      ROWS BETWEEN 6 PRECEDING AND CURRENT ROW
+    ) AS rolling_days,
+    SUM(amount) OVER(
+        ORDER BY visited_on
+        ROWS BETWEEN 6 PRECEDING AND CURRENT ROW
+    )AS rolling_sum,
+
+    AVG(amount) OVER(
+        ORDER BY visited_on
+        ROWS BETWEEN 6 PRECEDING AND CURRENT ROW
+    )AS rolling_average
+
+    FROM cte
+    ORDER BY visited_on
+)
+SELECT visited_on,rolling_sum AS amount,ROUND(rolling_average,2) AS average_amount
+FROM final
+WHERE rolling_days = 7
 --ORDER BY visited_on
 
 --current day + 6 days before
