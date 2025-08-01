0af098dfd7e3dc8da36e480447414e52f0e0d8b1
JibonKrishna <jibon.mbox@outlook.com>
2025-08-02 05:05:41 +0600

    Added another implementation

--- LeetCode_SQL_50/LeetCode_176.sql
+++ LeetCode_SQL_50/LeetCode_176.sql
@@ -6,3 +6,12 @@ WITH cte as(
 )
 
 select MAX(salary) AS SecondHighestSalary from cte where rank = 2
+
+
+-- Another Implementation
+
+-- Write your PostgreSQL query statement below
+SELECT (SELECT DISTINCT salary
+FROM Employee
+ORDER BY salary DESC
+LIMIT 1 OFFSET 1) AS SecondHighestSalary
